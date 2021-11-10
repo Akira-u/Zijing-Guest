6aa7c26945beebca066f8a359b1fdf3c123d43dd
Ray1ee01 <603957941@qq.com>
2021-11-10 19:53:07 +0800

    build first model and viewset

--- backend/guard/views.py
+++ backend/guard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import LogSerializer
 # Create your views here.
+
+from guard.models import Log
+class LogViewSet(viewsets.ModelViewSet):
+    """ 来访日志 viewset """
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
