96d91078002832e67f90e4ad599dc858dfc7c95b
Ray1ee01 <603957941@qq.com>
2021-11-10 23:35:58 +0800

    add pk into response

--- backend/guard/serializers.py
+++ backend/guard/serializers.py
@@ -5,6 +5,7 @@ class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
         fields = [
+            'id', # primary key
             'name',
             'custom_id',
         ]
