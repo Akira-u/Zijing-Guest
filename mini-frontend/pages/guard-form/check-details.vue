2b10dd4a4364ee90754644a16eb2640b2689dd3b
Matsu-0 <1147172850@qq.com>
2021-12-04 14:21:15 +0800

    remind message

--- mini-frontend/pages/guard-form/check-details.vue
+++ mini-frontend/pages/guard-form/check-details.vue
@@ -28,15 +28,24 @@
     <view class="buttonList">
       <button @tap="Remind">提醒</button>
     </view>
+    <uni-popup class="remindPopup" ref="remindInput" type="dialog">
+      <uni-popup ref="popupMessage" type="message">
+			  <uni-popup-message :type="msgType" :message="errMsg" :duration="1500" />
+		  </uni-popup>
+      <uni-popup-dialog class="remindDialog" mode="input" title="提醒" value="同学您好，您的预计访问时间即将到达，请尽快签离。" placeholder="请输入提醒信息" before-close=true @close="dialogClose" @confirm="dialogInput"/>
+    </uni-popup>
   </view>
 </template>
 
 <script>
+import {registeredGuardRequest} from "@/api/request";
 import { decodeOption } from "@/api/navigate";
 export default {
   data() {
     return {
       log: {},
+      remindMsg: '',
+      errMsg: 'error message',
     };
   },
   onLoad(options) {
@@ -58,8 +67,48 @@ export default {
     },
     Remind() {
       console.log("remind");
+      this.$refs["remindInput"].open()
       //TO DO
     },
+    dialogInput(remindMessage) {
+      this.remindMsg = remindMessage;
+			
+      if (this.remindMsg.length > 25) {
+        this.errMsg = "提醒信息长度应不多于25个字符"
+        this.$refs.popupMessage.open();
+      }
+      else if (this.remindMsg.length == 0) {
+        this.errMsg = "提醒信息不能为空"
+        this.$refs.popupMessage.open();
+      }
+      else{
+        console.log(this.remindMsg)
+        this.$refs["remindInput"].close()
+        registeredGuardRequest({
+          url: "/guard/remind/",
+          method: "POST",
+          data: {
+		        open_id: this.log.guest_id,
+            msg: {
+              thing6: {
+                value: this.log.guest_name
+              },
+              thing1: {
+                value: this.log.target_dorm
+              },
+              thing5: {
+                value: this.remindMsg
+              }
+            }
+          },
+        }).then((res) => {
+          console.log({ res: res });
+        });
+      }
+    },
+    dialogClose() {
+      this.$refs["remindInput"].close()
+    }
   },
 };
 </script>
@@ -107,4 +156,9 @@ button {
   margin: 20px;
   box-shadow: 0 5px 7px 0 rgba(86, 119, 252, 0.2);
 }
+
+input {
+  height: 200px;
+}
+
 </style>
