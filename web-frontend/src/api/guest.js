e2610f5588d089020cabbe6a0c05ab0073ea4a64
Ray1ee01 <66430550+Ray1ee01@users.noreply.github.com>
2021-12-09 19:28:13 +0800

    add guest api

---
+++ web-frontend/src/api/guest.js
@@ -0,0 +1,25 @@
+import request from '@/utils/request'
+
+export function getList(query) {
+    return request({
+      url: 'http://49.232.106.46:8000/guest',
+      method: 'get',
+      params: query
+    })
+  }
+export function toBlackList(data) {
+    console.log(data)
+    return request({
+      url: 'http://49.232.106.46:8000/guest/to_black/',
+      method: 'post',
+      data
+    })
+  }
+
+export function toWhiteList(data) {
+    return request({
+      url: 'http://49.232.106.46:8000/guest/to_white/',
+      method: 'post',
+      data
+    })
+  }
