1db279d52c166e661300faf7b7695528c08b8e2d
Matsu-0 <1147172850@qq.com>
2021-12-10 12:24:01 +0800

    replaced input with combox

--- mini-frontend/pages/guest-form/guest-form.vue
+++ mini-frontend/pages/guest-form/guest-form.vue
@@ -18,18 +18,21 @@
           placeholder="您的电话号码"
         />
       </uni-forms-item>
-      <uni-forms-item required label="目的楼号" name="target_building">
+      <uni-forms-item required label="到访楼号" name="target_building">
+        <view class="targetBuilding">
         <uni-combox 
-          :candidates="candidates" 
+          :candidates="building_list" 
           v-model="form_data.target_building" 
           placeholder="请选择到访宿舍楼号"
-        />
+          @input="changeInfo"
+        /></view>
       </uni-forms-item>
-      <uni-forms-item required label="目的宿舍" name="target_dorm">
-        <!-- TODO: combobox -->
-        <uni-easyinput
-          v-model="form_data.target_dorm"
-          placeholder="您要拜访的宿舍号"
+      <uni-forms-item required label="到访宿舍" name="target_dorm">
+        <uni-combox 
+          class="targetDorm"
+          :candidates="dorm_list" 
+          v-model="form_data.target_dorm" 
+          placeholder="请选择到访宿舍门牌号"
         />
       </uni-forms-item>
       <uni-forms-item required label="接待人" name="host_student">
@@ -66,15 +69,15 @@ export default {
     return {
       form_data: {
         phone: "13919198100",
-        dorm_id: "1号楼",
-        target_dorm: '403A',
+        target_building: '',
+        target_dorm: '',
         host_student: '李端',
         purpose: '拿杯'
       },
       msg_text:"您好，欢迎。",
-      candidates:[
-        "紫荆1号楼","紫荆2号楼"
-      ],
+      building_list:[],
+      building_info:[],
+      dorm_list:[],
       rules: {
         // 表单验证
         phone: {
@@ -124,7 +127,6 @@ export default {
             if(res.phone){
               that.form_data.phone = res.phone;
             }
-            console.log("res:",res.name)
             that.msg_text = res.name + " 您好，"
             if(res.is_student) {
               that.msg_text += "您以 学生 身份登录。"
@@ -144,6 +146,15 @@ export default {
         if (res.status === 'still in') {
           // if user is in dorm, jump to in-dorm page directly
           uni.redirectTo({ url: '/pages/guest-form/in-dorm' })
+        }
+      })
+    request({ url: "/dormbuilding/" })
+      .then((res) => {
+        that.building_list = []
+        that.building_info = res.results
+        var len = res.count
+        for(var j = 0; j < len; j++) {
+          that.building_list.push(res.results[j].name)
         }
       })
   },
@@ -178,6 +189,24 @@ export default {
     viewHistory() {
       navigateTo("/pages/guest-form/my-history")
     },
+    changeInfo() {
+      var that = this
+      var building = that.form_data.target_building
+      var index = that.building_list.findIndex(i => i == building)
+      if (index > -1){
+        index = that.building_info[index].id
+        var url = "/dorm/?dormbuilding_id=" + index
+        request({ url: url })
+          .then((res) => {
+            console.log("dorm",res)
+            var len = res.count
+            that.dorm_list=[]
+            for(var j = 0; j < len; j++) {
+              that.dorm_list.push(res.results[j].name)
+            }
+          })
+      } 
+    }
   },
 };
 </script>
@@ -197,6 +226,7 @@ export default {
   background: #f8f7fc;
   font-size: 30rpx;
   border-radius: 10rpx;
+  position:absolute;
 }
 
 .uni-forms-item {
@@ -204,10 +234,19 @@ export default {
 }
 
 .uni-combox {
-  z-index: 99;
   background: #f8f7fc;
   font-size: 30rpx;
   border-radius: 10rpx;
+}
+
+.targetBuilding {
+  position: absolute;
+  z-index:99;
+}
+
+.targetDorm {
+  position: absolute;
+  z-index:98;
 }
 
 button {
